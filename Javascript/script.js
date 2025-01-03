934cdb953810a191d9d9f13d7f0ce905f5705f17
Nyazashali <Shaik.nyazashali@gmail.com>
2025-01-03 22:22:43 +0530

    Javascript

---
+++ Javascript/script.js
@@ -0,0 +1,36 @@
+let variableName = 'Welcome to javascript';
+variableName = 'Hello';
+
+console.log(variableName);
+
+//there are 3 different ways to declare a variable
+//let, const, var is used to store & declare the value
+//"variableName" (a string literal) to the console.log function. This will print the literal text "variableName" to the console, 
+// not the value stored in the variableName variable.
+
+//To print the value of the variable variableName,
+//  you should remove the quotes around the variable name
+
+//let is used to reassin the value 
+//first we gave a default value, then we reassigned the variable or just we gave it a different value to the same variable 
+// we cannot reassign the value using const.
+
+const variableName1 = 'Welcome to javascript';
+const variableName2 = 'Javascript';
+console.log(variableName1);
+console.log(variableName1);
+
+const $name = 'John';
+console.log($name);
+
+//$, _ can be used for variable names
+
+var A = "Test0";
+let B = 'Test1';
+const C = 'Test2';
+
+console.log(A);
+console.log(B);
+console.log(C);
+
+
